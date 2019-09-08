b064dc82722548d7931628c8559b391471ba5268
Student_20194 <fengtianshi@hotmail.com>
2019-09-08 16:43:21 -0400

    updata

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 HOST='192.168.200.52'
 PORT=19002
@@ -10,13 +11,17 @@ s.connect(ADDR)
 
 list=["Shi Tang","look mirror","get hairpin","unlock door with hairpin","open door"]
 
-for i in range (4):
+for i in range(len (list)):
+    print(i)
     aa=s.recv(1024)
     b=aa.decode()
     print(b)
     n=list[i].encode()
     s.send(n)
+    time.sleep(0.25)
     i=i+1
+suc_res = s.recv(1024)
+print(suc_res.decode())
 
 s.close()
 
