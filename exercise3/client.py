58b51b8a6f264e0f61ffd1cafb9fa1e1fa43db50
唐实 <tangshi@tangshideMacBook-Air.local>
2019-09-22 19:42:13 -0400

    upgrade

--- exercise3/client.py
+++ exercise3/client.py
@@ -49,9 +49,9 @@ class EchoClientProtocol(asyncio.Protocol):
                     self.send(commond)
                     self.i += 1
                 else:
-                    if flag[1] == "hit":
-                        self.send(" ")
-                        time.sleep(1)
+                    if flag[1] == "flying":
+                        # self.send(" ")
+                        # time.sleep(1)
                         print(self.list[self.i])
                         commond = self.list[self.i]
                         self.send(commond)
