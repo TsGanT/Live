c7506375fcf9f2f23074c68ffd0dbee041127d72
TsGanT <41409723+TsGanT@users.noreply.github.com>
2019-11-15 21:08:49 -0500

    tts

--- crapTS/protocol.py
+++ crapTS/protocol.py
@@ -90,6 +90,7 @@ class CRAP(StackingProtocol):
         self.status = "LISTEN"
 
         if self._mode == "client":
+            print("connecton made")
             #generate RSA signKA and public key
             self.l_private_key = rsa.generate_private_key(public_exponent=65537,key_size=2048,
                 backend=default_backend())
