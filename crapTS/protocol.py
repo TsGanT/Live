1f4ea044162bd60c24e8d1451472ea00c815b1de
TsGanT <41409723+TsGanT@users.noreply.github.com>
2019-11-16 18:23:39 -0500

    tts

--- crapTS/protocol.py
+++ crapTS/protocol.py
@@ -76,7 +76,6 @@ class CRAP(StackingProtocol):
         logger.debug("{} CRAP: craptography protocol".format(mode))
         print("Verfying.......")
         self.mode = mode
-        self.status = 0
         self.pk = None
         self.signature = None
         self.cert = None
@@ -96,8 +95,6 @@ class CRAP(StackingProtocol):
 
 
         #There are some codes about sign the public key
-
-        self.status = "LISTEN"
 
         if self.mode == "client":
             print("connecton made")
@@ -146,7 +143,6 @@ class CRAP(StackingProtocol):
             self.transport.write(handshake_pkt.__serialize__())
             print("client already sent")
             #self.handshake_timeout_task = self.loop.create_task(self.handshake_timeout_check())
-            self.status = "PK_SENT"
     
     def handshake_send_error(self):
         print("handshake error!")
@@ -167,121 +163,118 @@ class CRAP(StackingProtocol):
             if pkt.status == 2:
                 logger.debug("{}, CRAP: ERROR: recv an error packet ".format(self.mode))
                 return
-            elif self.status == "LISTEN":
+            elif pkt.status == 0:
                 print("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL")
-                if pkt.status == 0:
-                    # We need to transfer bytes in to object
-                    print("get packet")
-                    Acert = x509.load_pem_x509_certificate(pkt.cert, default_backend())
-                    spublic_keyA = Acert.public_key()
-                    print("Server get cert from client:")
-                    print(Acert)
-                    try:
-                        print("begin verify client's signature")
-                        spublic_keyA.verify(
-                            pkt.signature,
-                            pkt.pk,
-                            padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),
-                            hashes.SHA256()
-                        )
-                    except Exception as error:
-                        logger.debug("Wrong signature from client!!!!!!!")
-                        handshake_pkt = HandshakePacket(status=2)
-                        self.transport.write(handshake_pkt.__serialize__())
-                        self.transport.close()
-
-                    self.privatekB = ec.generate_private_key(ec.SECP384R1(), default_backend())
-                    self.pk = self.privatekB.public_key()
-
-                    self.l_private_keyB = rsa.generate_private_key(public_exponent=65537,key_size=2048,
-                        backend=default_backend())
-                    self.l_public_keyB = self.l_private_keyB.public_key()
-                    subject = issuer = x509.Name([x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
-                        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"California"),
-                        x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
-                        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"My Company"),
-                        x509.NameAttribute(NameOID.COMMON_NAME, u"mysite.com"),])
-                    certB = x509.CertificateBuilder().subject_name(subject).issuer_name(
-                        issuer).public_key(
-                            self.l_public_keyB).serial_number(x509.random_serial_number()
-                                ).not_valid_before(datetime.datetime.utcnow()).not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=10)).add_extension(
-                                    x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),critical=False,).sign(self.l_private_keyB, hashes.SHA256(), default_backend())
-                    certB_bytes = certB.public_bytes(Encoding.PEM)
-
-                    self.nonceB = random.randrange(0, 2**10) #os.urandom(32)
-                    nonceSignatureB = self.l_private_keyB.sign(
-                        str(pkt.nonce).encode('ASCII'),
-                        padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
-                        salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256()
+                # We need to transfer bytes in to object
+                print("get packet")
+                Acert = x509.load_pem_x509_certificate(pkt.cert, default_backend())
+                spublic_keyA = Acert.public_key()
+                print("Server get cert from client:")
+                print(Acert)
+                try:
+                    print("begin verify client's signature")
+                    spublic_keyA.verify(
+                        pkt.signature,
+                        pkt.pk,
+                        padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),
+                        hashes.SHA256()
                     )
-
-                    self.pk_bytes = self.pk.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo)
-                    self.signature = self.l_private_keyB.sign(
-                        self.pk_bytes,
-                        padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
-                        salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256()
-                    )       #Get signature
-                    self.cert = certB_bytes
-                    handshake_pkt = HandshakePacket(pk = self.pk_bytes, status = 1, nonce = self.nonceB, 
-                        nonceSignature=nonceSignatureB,
-                        signature = self.signature, cert = self.cert)    # This is peer2 first get packet
+                except Exception as error:
+                    logger.debug("Wrong signature from client!!!!!!!")
+                    handshake_pkt = HandshakePacket(status=2)
                     self.transport.write(handshake_pkt.__serialize__())
+                    self.transport.close()
 
-                    publickeyA = load_pem_public_key(pkt.pk, backend=default_backend())
-                    server_shared_key = self.privatekB.exchange(ec.ECDH, publickeyA)#Alreday calcualte
-                    self.status = "PK_SENT"
-                elif pkt.status == 1:
-                    try:
-                        print("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL")
-                        spublic_keyA.verify(pkt.nonceSignature, self.nonceB,
-                            padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),
-                            hashes.SHA256())
+                self.privatekB = ec.generate_private_key(ec.SECP384R1(), default_backend())
+                self.pk = self.privatekB.public_key()
 
-                    except Exception as error:
-                        logger.debug("Sever verify failed because wrong signature")
-                        handshake_pkt = HandshakePacket(status=2)
-                        self.transport.write(handshake_pkt.__serialize__())
-                        self.transport.close()
-                    print("Handshake complete")
-                    
+                self.l_private_keyB = rsa.generate_private_key(public_exponent=65537,key_size=2048,
+                    backend=default_backend())
+                self.l_public_keyB = self.l_private_keyB.public_key()
+                subject = issuer = x509.Name([x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+                    x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"California"),
+                    x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
+                    x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"My Company"),
+                    x509.NameAttribute(NameOID.COMMON_NAME, u"mysite.com"),])
+                certB = x509.CertificateBuilder().subject_name(subject).issuer_name(
+                    issuer).public_key(
+                        self.l_public_keyB).serial_number(x509.random_serial_number()
+                            ).not_valid_before(datetime.datetime.utcnow()).not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=10)).add_extension(
+                                x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),critical=False,).sign(self.l_private_keyB, hashes.SHA256(), default_backend())
+                certB_bytes = certB.public_bytes(Encoding.PEM)
+
+                self.nonceB = random.randrange(0, 2**10) #os.urandom(32)
+                nonceSignatureB = self.l_private_keyB.sign(
+                    str(pkt.nonce).encode('ASCII'),
+                    padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256()
+                )
+
+                self.pk_bytes = self.pk.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo)
+                self.signature = self.l_private_keyB.sign(
+                    self.pk_bytes,
+                    padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256()
+                )       #Get signature
+                self.cert = certB_bytes
+                handshake_pkt = HandshakePacket(pk = self.pk_bytes, status = 1, nonce = self.nonceB, 
+                    nonceSignature=nonceSignatureB,
+                    signature = self.signature, cert = self.cert)    # This is peer2 first get packet
+                self.transport.write(handshake_pkt.__serialize__())
+
+                publickeyA = load_pem_public_key(pkt.pk, backend=default_backend())
+                server_shared_key = self.privatekB.exchange(ec.ECDH, publickeyA)#Alreday calcualte
+            elif pkt.status == 1:
+                try:
+                    print("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL")
+                    spublic_keyA.verify(pkt.nonceSignature, self.nonceB,
+                        padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),
+                        hashes.SHA256())
+
+                except Exception as error:
+                    logger.debug("Sever verify failed because wrong signature")
+                    handshake_pkt = HandshakePacket(status=2)
+                    self.transport.write(handshake_pkt.__serialize__())
+                    self.transport.close()
+                print("Handshake complete")
+                
                 else:
                     #ERROR: This is peer2 and the first time the status cannot be 1 or 2
                     self.handshake_send_error()
                     return
             
-        elif self.status == "PK_SENT" and pkt.status == 1:
+        elif self.mode == "client" and pkt.status == 1:
             #peer1 or peer2 has sent his public key and try to first verify cert and second calculate the shared key
             #There are some codes about verify the cert 
-            if self.mode == "client":
-                if pkt.status == 1:     #peer1 first get the public key from peer2
-                    Bcert = x509.load_pem_x509_certificate(pkt.cert, default_backend())
-                    spublic_keyB = Bcert.public_key()
-                    try:
-                        print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-                        spublic_keyB.verify(pkt.signature, pkt.pk,
-                            padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH),
-                            hashes.SHA256())
-                        spublic_keyB.verify(pkt.nonceSignature, self.nonceA,
-                            padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH),
-                            hashes.SHA256())
+            if pkt.status == 1:     #peer1 first get the public key from peer2
+                Bcert = x509.load_pem_x509_certificate(pkt.cert, default_backend())
+                spublic_keyB = Bcert.public_key()
+                try:
+                    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+                    spublic_keyB.verify(pkt.signature, pkt.pk,
+                        padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH),
+                        hashes.SHA256())
+                    spublic_keyB.verify(pkt.nonceSignature, self.nonceA,
+                        padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH),
+                        hashes.SHA256())
 
-                    except Exception as error:
-                        logger.debug("Sever verify failed because wrong signature")
-                        handshake_pkt = HandshakePacket(status=2)
-                        self.transport.write(handshake_pkt.__serialize__())
-                        self.transport.close()               
-                
-                    publickeyB = load_pem_public_key(pkt.pk, backend=default_backend())
-                    client_shared_key = self.privatekA.exchange(ec.ECDH, publickeyB)
-                    nonceSignatureA = self.l_private_key.sign(pkt.nonce,
-                                        padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),
-                                        hashes.SHA256())
-
-                    handshake_pkt = HandshakePacket(status=1, nonceSignature=nonceSignatureA)
+                except Exception as error:
+                    logger.debug("Sever verify failed because wrong signature")
+                    handshake_pkt = HandshakePacket(status=2)
                     self.transport.write(handshake_pkt.__serialize__())
-                else:
-                    self.handshake_send_error()
-                    return
+                    self.transport.close()               
+            
+                publickeyB = load_pem_public_key(pkt.pk, backend=default_backend())
+                client_shared_key = self.privatekA.exchange(ec.ECDH, publickeyB)
+                nonceSignatureA = self.l_private_key.sign(pkt.nonce,
+                                    padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),
+                                    hashes.SHA256())
+
+                handshake_pkt = HandshakePacket(status=1, nonceSignature=nonceSignatureA)
+                self.transport.write(handshake_pkt.__serialize__())
+            else:
+                self.handshake_send_error()
+                return
     
 SecureClientFactory = StackingProtocolFactory.CreateFactoryType(
     lambda: POOP(mode="client"), lambda: CRAP(mode="client"))
